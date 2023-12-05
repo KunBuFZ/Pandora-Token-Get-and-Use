a1c11f583905a394a5c104632491e1d58ed97c90
KunBuFZ <152124473+KunBuFZ@users.noreply.github.com>
2023-12-05 22:01:35 +0800

    添加session_token支持

--- web/固定base_url/app.py
+++ web/固定base_url/app.py
@@ -22,6 +22,7 @@ def get_tokens():
     response = requests.post(login_url, data=data)
     if response.status_code == 200:
         access_token = response.json().get('access_token')
+        session_token = response.json().get('session_token')
 
         # 使用 access token 获取 share token
         register_url = f"{base_url}/api/token/register"
@@ -30,7 +31,7 @@ def get_tokens():
         response = requests.post(register_url, headers=headers, data=data)
         if response.status_code == 200:
             share_token = response.json().get('token_key')
-            return f"Access Token: {access_token}<br>Share Token: {share_token}"
+            return f"Access Token: {access_token}<br><br><br>Session Token: {session_token}<br><br><br>Share Token: {share_token}"
         else:
             return "Failed to get share token."
     else:
